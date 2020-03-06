83280549b7d97496a2ce142126b4de1dc0dde4fa
stas <kapilka06@gmail.com>
2020-03-06 10:58:27 +0300

    abstract DTO class added, BookDTO and spatie/data-transfer-object

---
+++ app/src/Containers/Book/DTO/BookDTO.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LargeLaravel\Containers\Book\DTO;
+
+use Carbon\Carbon;
+use LargeLaravel\Core\Abstracts\DTO\DataTransferObject;
+
+class BookDTO extends DataTransferObject
+{
+    public int $id;
+    public string $title;
+    public string $original_title;
+    public int $author_id;
+    public string $description;
+    public string $image_guid;
+    public int $cover_type_id;
+    public int $num_of_pages;
+    public Carbon $publish_date;
+    public int $publisher_id;
+    public string $ISBN;
+    public string $edition;
+}
---
+++ app/src/Core/Abstracts/DTO/DataTransferObject.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace LargeLaravel\Core\Abstracts\DTO;
+
+use Spatie\DataTransferObject\DataTransferObject as SpatieDTO;
+
+abstract class DataTransferObject extends SpatieDTO
+{
+
+}
