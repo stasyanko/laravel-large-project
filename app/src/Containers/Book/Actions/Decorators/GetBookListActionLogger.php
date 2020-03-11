fcb8c6ed65fe69a264a7fea1bde3e23cf4880757
stas <kapilka06@gmail.com>
2020-03-11 11:50:23 +0300

    fixed GetBookListActionLogger

--- app/src/Containers/Book/Actions/Decorators/GetBookListActionLogger.php
+++ app/src/Containers/Book/Actions/Decorators/GetBookListActionLogger.php
@@ -11,7 +11,7 @@ class GetBookListActionLogger extends GetBookListActionDecorator
 {
     public function execute(PaginateRequestInterface $paginateRequest): BookCollection
     {
-        $bookCollection = parent::execute($paginateRequest);
+        $bookCollection = $this->getBookListActionInterface->execute($paginateRequest);
         \Log::info("returned " . count($bookCollection) . " books");
 
         return $bookCollection;
