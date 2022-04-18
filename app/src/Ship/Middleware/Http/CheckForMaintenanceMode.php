967e61127bebc766584d8b04bf5221ad98dfd8b3
Shift <shift@laravelshift.com>
2022-04-18 15:20:26 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-2 coding style as a base with some minor additions.
    
    You may customize the code style applied by adding a [PHP CS Fixer][1] or [PHP CodeSniffer][2] ruleset to your project root. Feel free to use [Shift's Laravel ruleset][3] to help you get started.
    
    For more information on customizing the code style applied by Shift, [watch this short video][4].
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://github.com/squizlabs/PHP_CodeSniffer
    [3]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200
    [4]: https://laravelshift.com/videos/shift-code-style

--- app/src/Ship/Middleware/Http/CheckForMaintenanceMode.php
+++ app/src/Ship/Middleware/Http/CheckForMaintenanceMode.php
@@ -4,7 +4,7 @@ namespace LargeLaravel\Ship\Middleware\Http;
 
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
 
-class PreventRequestsDuringMaintenance extends Middleware
+class CheckForMaintenanceMode extends Middleware
 {
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
